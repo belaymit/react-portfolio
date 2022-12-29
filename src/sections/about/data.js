4f90c313044c7b90bf5b5ce5bdb11d6cb8169cf0
belaymit <2belamit@gmail.com>
2022-12-29 14:52:09 +0300

    added dynamic data and icons to cards component

---
+++ src/sections/about/data.js
@@ -0,0 +1,17 @@
+import { FaAward } from 'react-icons/fa';
+import { TbBooks } from 'react-icons/tb';
+import { BiHappyHeartEyes } from 'react-icons/bi';
+
+const data = [
+  {
+    id: 1, icon: <FaAward />, title: 'Experience', desc: '1+ Years Working',
+  },
+  {
+    id: 1, icon: <TbBooks />, title: 'Projects', desc: '20+ Completed',
+  },
+  {
+    id: 1, icon: <BiHappyHeartEyes />, title: 'Client', desc: '20+ happy clients',
+  },
+];
+
+export default data;
