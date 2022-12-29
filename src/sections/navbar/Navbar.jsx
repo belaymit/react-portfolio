a11e3dda5956ec44d9b50c0aa14553794cf9b6de
belaymit <2belamit@gmail.com>
2022-12-29 07:42:52 +0300

    added man-items and logo

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { IoIosColorFilter } from 'react-icons/io';
+import Logo from '../../assets/logo.PNG';
+import menuList from './menuList';
 import './navbar.css';
 
 const Navbar = () => (
-  <nav>Navbar</nav>
+  <nav>
+    <div className="container nav-container">
+      <a href="index.html" className="nav-logo">
+        <img src={Logo} alt="logo" />
+      </a>
+      <ul className="nav-menu">
+        {menuList.map((item) => (
+          <li key={item.id}>
+            <a href={item.link}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+      <button type="button" id="them-icon">
+        <IoIosColorFilter />
+      </button>
+    </div>
+  </nav>
 );
 
 export default Navbar;
