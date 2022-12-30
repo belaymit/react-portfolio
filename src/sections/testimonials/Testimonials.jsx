90ea3805764b7c073116192f428e3cfbe53b926c
belaymit <2belamit@gmail.com>
2022-12-30 07:55:01 +0300

    added images and testimonials to testimonial section

--- src/sections/testimonials/Testimonials.jsx
+++ src/sections/testimonials/Testimonials.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import testimonialData from './testimonialsData';
 import './testimonials.css';
 
 const Testimonials = () => (
-  <section id="testimonials">Testimonials</section>
+  <section id="testimonials" className="testimonials">
+    <h5 className="review-title">Review from Reviewers</h5>
+    <h2>Testimonials</h2>
+    <div className="container testimonial-container">
+      {
+        testimonialData.map(({
+          avatar, name, organization, review,
+        }, index) => (
+          // eslint-disable-next-line react/jsx-key, react/no-array-index-key
+          <article className="testimonial" key={index}>
+            <div className="reviewer-avatar">
+              <img src={avatar} alt={name} />
+            </div>
+            <h5 className="reviewer-name">{name}</h5>
+            <span>{organization}</span>
+            <small className="reviewer-review">
+              {review}
+            </small>
+          </article>
+        ))
+      }
+    </div>
+  </section>
 );
 
 export default Testimonials;
