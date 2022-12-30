fd88cc49b0a2df0237b01aeaf0e1519e5bddb703
belaymit <2belamit@gmail.com>
2022-12-30 07:48:36 +0300

    added icons to floating navigation

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import './floatingnav.css';
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai';
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi';
+import { RiServiceLine } from 'react-icons/ri';
+import { MdModelTraining } from 'react-icons/md';
+import './nav.css';
 
 const FloatingNav = () => (
-  <section id="floating-nav">Floating-nav</section>
+  <section id="floating-nav" className="floating-nav">
+    <a href="/#">
+      <AiOutlineHome />
+    </a>
+    <a href="#about">
+      <AiOutlineUser />
+    </a>
+    <a href="#experience">
+      <BiBook />
+    </a>
+    <a href="#services">
+      <RiServiceLine />
+    </a>
+    <a href="#portfolio">
+      <MdModelTraining />
+    </a>
+    <a href="#contacts">
+      <BiMessageSquareDetail />
+    </a>
+  </section>
 );
 
 export default FloatingNav;
