c2d7fed571dc05d277fd17ea774e17667c1ed05c
belaymit <2belamit@gmail.com>
2022-12-30 07:56:12 +0300

    added array ot testimonials dynamically

---
+++ src/sections/testimonials/testimonialsData.js
@@ -0,0 +1,40 @@
+import Alan from '../../assets/Alan-Turing.jfif';
+import Ariel from '../../assets/Ariel-Camus.jfif';
+import Tim from '../../assets/Time-Berners-Lee.jfif';
+import Surgy from '../../assets/Surgy-Brin.jfif';
+
+const testimonialData = [
+  {
+    avatar: Alan,
+    name: 'Allan Turing',
+    organization: 'Father of AI',
+    review:
+    'In this context, the term "software product" means "any technical document or partial document, produced as a deliverable of a software development activity", and may include documents such as contracts, project plans and budgets, requirements documents, specifications, designs, source code, user documentation, support and maintenance documentation, test plans, test specifications, standards, and any other type of specialist work product.',
+  },
+
+  {
+    avatar: Ariel,
+    name: 'Ariel Camus',
+    organization: 'Founder of Microverse',
+    review:
+    'In this context, the term "software product" means "any technical document or partial document, produced as a deliverable of a software development activity", and may include documents such as contracts, project plans and budgets, requirements documents, specifications, designs, source code, user documentation, support and maintenance documentation, test plans, test specifications, standards, and any other type of specialist work product.',
+  },
+
+  {
+    avatar: Tim,
+    name: 'Tim Berners-Lee',
+    organization: 'Founder of WWW',
+    review:
+    'In this context, the term "software product" means "any technical document or partial document, produced as a deliverable of a software development activity", and may include documents such as contracts, project plans and budgets, requirements documents, specifications, designs, source code, user documentation, support and maintenance documentation, test plans, test specifications, standards, and any other type of specialist work product.',
+  },
+
+  {
+    avatar: Surgy,
+    name: 'Surgy Brin',
+    organization: 'Founder of Google',
+    review:
+    'In this context, the term "software product" means "any technical document or partial document, produced as a deliverable of a software development activity", and may include documents such as contracts, project plans and budgets, requirements documents, specifications, designs, source code, user documentation, support and maintenance documentation, test plans, test specifications, standards, and any other type of specialist work product.',
+  },
+
+];
+export default testimonialData;
